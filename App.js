5d3336b52a7d373fe76e7e7b426f09fc12c0db6b
nartco <sebastien.thuillier@hotmail.com>
2020-06-21 17:55:59 +0200

    add goalinput & item

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
+
+import GoalItem from './components/Goalitems'
+import GoalInput from './components/Goalinput'
 
 export default function App() {
-  return (
-    <View style={{ padding: 30}}>
-      <View>
-        <TextInput />
-        <Button title='ADD'/>
-      </View>
-      <View>
+  const [courseGoals, setCourseGoals] = useState([]);
 
-      </View>
+
+  const addGoal = goalTitle =>
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle}]);
+  return (
+    <View style={styles.screen}>
+      <GoalInput addGoal={addGoal}/>
+      <FlatList
+        keyExtractor={(item,index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
 });
